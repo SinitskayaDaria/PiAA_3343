c7a2f55ff2b321ecbe2c03b1b8739bd8527fdbd8
SinitskayaDaria <144446685+SinitskayaDaria@users.noreply.github.com>
2025-04-10 16:56:46 +0300

    Update lr_3_1.cpp

--- LR_3/Programm/lr_3_1.cpp
+++ LR_3/Programm/lr_3_1.cpp
@@ -2,8 +2,39 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
+
+// метод печати таблицы расстояний между 
+void printDPTable(const vector<vector<int>>& dp, const string& A, const string& B, int m, int n) {
+
+    cout << "\nТаблица расстояний (dp):\n";
+    cout << setw(6) << " "; 
+    cout << setw(6) << "' '"; 
+
+    // заголовок столбцов для строки B
+    for (char ch : B)
+        cout << setw(6) << ch; // печать каждого символа строки B с отступом
+    cout << endl;
+
+    // строки для каждого символа строки A
+    for (int i = 0; i <= m; ++i) {
+        // если это первая строка 
+        if (i == 0)
+            cout << setw(6) << "' '"; // пустое значение
+        else
+            cout << setw(6) << A[i - 1]; // символ строки A с отступом
+
+        // все значения таблицы для текущей строки
+        for (int j = 0; j <= n; ++j) {
+            cout << setw(6) << dp[i][j]; // текущая ячейка с отступом
+        }
+        cout << endl; 
+    }
+    cout << endl; 
+}
+
 
 // метод инициализации базовых случаев для динамической таблицы
 void initializeBaseCases(vector<vector<int>>& dp, int m, int n, int delete_cost, int insert_cost) {
@@ -14,6 +45,9 @@ void initializeBaseCases(vector<vector<int>>& dp, int m, int n, int delete_cost,
     // заполнение первой (число операций вставки символов)
     for (int j = 0; j <= n; ++j)
         dp[0][j] = j * insert_cost; // стоимость вставки j символов
+
+    cout << "\nПосле инициализации базовых случаев:\n";
+    printDPTable(dp, "", "", m, n);
 }
 
 // метод вычисления редакционного расстояния между строками A и B
@@ -26,6 +60,9 @@ void computeEditDistance(vector<vector<int>>& dp, const string& A, const string&
             // если текущие символы совпадают
             if (A[i - 1] == B[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1]; // стоимость остается прежней
+
+                cout << "dp[" << i << "][" << j << "] = dp[" << i-1 << "][" << j-1 << "] (символы совпадают '" << A[i-1] << "') = " << dp[i][j] << "\n";
+            
             } else {
 
                 // выбор минимальной стоимости из трех возможных операций
@@ -34,9 +71,17 @@ void computeEditDistance(vector<vector<int>>& dp, const string& A, const string&
                     dp[i][j - 1] + insert_cost,      // стоимость вставки символа
                     dp[i - 1][j] + delete_cost       // стоимость удаления символа
                 });
+
+                cout << "dp[" << i << "][" << j << "] = min(замена: " << dp[i - 1][j - 1] + replace_cost
+                << ", вставка: " << dp[i][j - 1] + insert_cost
+                << ", удаление: " << dp[i - 1][j] + delete_cost 
+                << ") = " << dp[i][j] << "\n";
             }
         }
     }
+
+    cout << "\nФинальная таблица расстояний:\n";
+    printDPTable(dp, A, B, m, n);
 }
 
 int main() {
@@ -53,7 +98,7 @@ int main() {
     initializeBaseCases(dp, m, n, delete_cost, insert_cost);                        // инициализация базовых случаев
     computeEditDistance(dp, A, B, replace_cost, insert_cost, delete_cost, m, n);    // вычисление расстояния редактирования
 
-    cout << dp[m][n] << endl;
+    cout << "\nМинимальная стоимость преобразования: " << dp[m][n] << endl;
 
     return 0; 
 }
